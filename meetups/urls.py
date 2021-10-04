b116a8baa4411e60bb07336cbba96cc7429f09c6
KacperSylwar <kacper.sylwa@gmail.com>
2021-10-04 19:51:37 +0200

    Dizango

--- meetups/urls.py
+++ meetups/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
 from meetups import views
 
 urlpatterns = [
-    path('meetups/', views.index)  # our-domain.com/meetups
+    path('meetups/', views.index,name='news-year-archive' ),  # our-domain.com/meetups
+    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-detail'),
+    # our-domain.com/meetups/<dynamic-path-segment>
 ]
