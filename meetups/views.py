0b10a3e9e3dbfd16060742f1eb52074f33d8b06d
KacperSylwar <kacper.sylwa@gmail.com>
2021-10-04 18:58:04 +0200

    Dizango

--- meetups/views.py
+++ meetups/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'meetups/index.html')
+    meetups = [
+        {'title': 'A First Meetup',
+         'location': 'New York',
+         'slug': 'a-first-meetup'},
+        {'title': 'A Second Meetup',
+         'location': 'Paris',
+         'slug': 'a-second-meetup'}
+    ]
+    return render(request, 'meetups/index.html', {
+        'show_meetups': True,
+        'meetups': meetups
+    })
